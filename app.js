d321c648493eef896191a4261cff646a411e9500
Aaron Rincon <rlaaron0305@gmail.com>
2022-08-29 20:19:33 -0500

    error handler

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ const id2 = process.env.ID2;
 */
 const webhook1 = new WebhookClient({id: "1013201604326989964", token: "im2xMvDvflllcdmvcKNDjimwhExaQmJ7EkWmoi-dbiFM7fmjiIfcxJr9LsalCTglINVI"});
 const webhook2 = new WebhookClient({id: "1013532925364535346", token: "AAEZFoOGSeTcJP8wi6Ijc_do-1HpkVKQs-Zph3zZAX-6kDZAxlhHCeOSkWN_i5GqiCan"});
+const webhookErr = new WebhookClient({id: "1013967197590270022", token: "OlJFRgQ1ZVU8mXXjMV2nhbQzigbkXbg_4FnlfDqkwV5lrx673ZbACVLxobkIzvHApJro"});
+
 // add ids to the array
 const generalID = [ id1, id2]
 // add webhooks to the array
@@ -28,6 +30,7 @@ client.on("ready", ready => {
     console.log('Crypto Mirror ready')
 });
 
+
 /* Listening to the messages in the chats and sending them to the webhooks. */
 client.on("messageCreate", messageCreate => {
     /* Checking if the message is from a bot. If it is, it will return and not send the message to the
@@ -35,19 +38,28 @@ client.on("messageCreate", messageCreate => {
     if(messageCreate.author.bot) return;
     /* Checking if the message is from the chat that you want to listen to. If it is, it will send the
     message to the webhook. */
-    for(let i = 0; i < generalID.length; i++) {
-        if (messageCreate.channel.id === generalID[i]) {
-            let content1 = messageCreate.content ? messageCreate.content : { embeds: [messageCreate]};
-            let username2 = messageCreate.author.username ? messageCreate.author.username : { embeds: [messageCreate.embeds[0]]};
-            let avatar2 = messageCreate.author.avatarURL() ? messageCreate.author.avatarURL() : { embeds: [messageCreate.embeds[0]]};
-            // if the message is a img or a txt
-            try{
-                
-                content1 =  messageCreate.attachments.first().url
-            }catch{}
-            /* Sending the message to the webhook. */
-            webhooks[i].send({ content: content1, username: username2, avatarURL: avatar2 });           
+        for(let i = 0; i < generalID.length; i++) {
+            if (messageCreate.channel.id === generalID[i]) {
+                let content1 = messageCreate.content ? messageCreate.content : { embeds: [messageCreate]};
+                let username2 = messageCreate.author.username ? messageCreate.author.username : { embeds: [messageCreate.embeds[0]]};
+                let avatar2 = messageCreate.author.avatarURL() ? messageCreate.author.avatarURL() : { embeds: [messageCreate.embeds[0]]};
+                // if the message is a img or a txt
+                try{
+
+                    content1 =  messageCreate.attachments.first().url
+                }catch{}
+                /* Sending the message to the webhook. */
+                /* Trying to send the message to the webhook. If it fails, it will send the error
+                message to the webhookErr. */
+                try{
+                    if (typeof content1 !== 'string') throw new error(errorMessage);
+                    webhooks[i].send({ content: content1, username: username2, avatarURL: avatar2 });
+                }catch(error){
+                    let errString = JSON.stringify(content1);
+                    console.log(JSON.stringify(content1));
+                    webhookErr.send({ content: errString, username: username2, avatarURL: avatar2 });
+                }
+            }
         }
-    }
-    
 });
+
