a467a3fb0cbc061bb6fc5509ae56c043557b31c5
Aaron Rincon <rlaaron0305@gmail.com>
2022-09-19 20:27:41 -0500

    update: clean code img, && text with img,

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 /* Importing the discord.js-selfbot-v13 and discord.js libraries. */
 const { Client } = require("discord.js-selfbot-v13")
-const {WebhookClient} = require("discord.js")
+const { WebhookClient } = require("discord.js")
 require('dotenv').config();
 
 /* Getting the values from the .env file. */
@@ -11,12 +11,12 @@ const id2 = process.env.ID2;
 /* Creating a new webhook client for each webhook.
     You can add more webhooks to listen the message in more chats.
 */
-const webhook1 = new WebhookClient({id: "1013201604326989964", token: "im2xMvDvflllcdmvcKNDjimwhExaQmJ7EkWmoi-dbiFM7fmjiIfcxJr9LsalCTglINVI"});
-const webhook2 = new WebhookClient({id: "1013532925364535346", token: "AAEZFoOGSeTcJP8wi6Ijc_do-1HpkVKQs-Zph3zZAX-6kDZAxlhHCeOSkWN_i5GqiCan"});
-const webhookErr = new WebhookClient({id: "1013967197590270022", token: "OlJFRgQ1ZVU8mXXjMV2nhbQzigbkXbg_4FnlfDqkwV5lrx673ZbACVLxobkIzvHApJro"});
+const webhook1 = new WebhookClient({ id: "1013201604326989964", token: "im2xMvDvflllcdmvcKNDjimwhExaQmJ7EkWmoi-dbiFM7fmjiIfcxJr9LsalCTglINVI" });
+const webhook2 = new WebhookClient({ id: "1013532925364535346", token: "AAEZFoOGSeTcJP8wi6Ijc_do-1HpkVKQs-Zph3zZAX-6kDZAxlhHCeOSkWN_i5GqiCan" });
+const webhookErr = new WebhookClient({ id: "1013967197590270022", token: "OlJFRgQ1ZVU8mXXjMV2nhbQzigbkXbg_4FnlfDqkwV5lrx673ZbACVLxobkIzvHApJro" });
 
 // add ids to the array
-const generalID = [ id1, id2]
+const generalID = [id1, id2]
 // add webhooks to the array
 const webhooks = [webhook1, webhook2]
 
@@ -35,31 +35,30 @@ client.on("ready", ready => {
 client.on("messageCreate", messageCreate => {
     /* Checking if the message is from a bot. If it is, it will return and not send the message to the
     webhook. */
-    if(messageCreate.author.bot) return;
+    if (messageCreate.author.bot) return;
     /* Checking if the message is from the chat that you want to listen to. If it is, it will send the
     message to the webhook. */
-        for(let i = 0; i < generalID.length; i++) {
-            if (messageCreate.channel.id === generalID[i]) {
-                let content1 = messageCreate.content ? messageCreate.content : { embeds: [messageCreate]};
-                let username2 = messageCreate.author.username ? messageCreate.author.username : { embeds: [messageCreate.embeds[0]]};
-                let avatar2 = messageCreate.author.avatarURL() ? messageCreate.author.avatarURL() : { embeds: [messageCreate.embeds[0]]};
-                // if the message is a img or a txt
-                try{
+    for (let i = 0; i < generalID.length; i++) {
+        if (messageCreate.channel.id === generalID[i]) {
+            let content = messageCreate.content ? messageCreate.content : { embeds: [messageCreate] };
+            let username = messageCreate.author.username ? messageCreate.author.username : { embeds: [messageCreate.embeds[0]] };
+            let avatarURL = messageCreate.author.avatarURL() ? messageCreate.author.avatarURL() : { embeds: [messageCreate.embeds[0]] };
+            let contImg = messageCreate.attachments.size > 0 ? messageCreate.attachments.first().url : null;
 
-                    content1 =  messageCreate.attachments.first().url
-                }catch{}
-                /* Sending the message to the webhook. */
-                /* Trying to send the message to the webhook. If it fails, it will send the error
-                message to the webhookErr. */
-                try{
-                    if (typeof content1 !== 'string') throw new error(errorMessage);
-                    webhooks[i].send({ content: content1, username: username2, avatarURL: avatar2 });
-                }catch(error){
-                    let errString = JSON.stringify(content1);
-                    console.log(JSON.stringify(content1));
-                    webhookErr.send({ content: errString, username: username2, avatarURL: avatar2 });
-                }
+            // check if the message has a txt
+            contImg ? messageCreate.content? content += `\n${contImg}` : content = contImg: null; 
+            // check if is a pinned message
+            messageCreate.type === 'CHANNEL_PINNED_MESSAGE' ? content = `:pushpin: PINNED_MESSAGE` : null;
+            
+            // send the message to the webhook
+            try{
+                webhooks[i].send({content, username, avatarURL });
+            }catch(err){
+                let errString = JSON.stringify(content);
+                errString = `Error: ${err}\n Objeto: \n${errString}`;
+                webhookErr.send({ content: errString, username, avatarURL });
             }
         }
+    }
 });
 
