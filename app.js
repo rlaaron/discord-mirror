08d96e106521c4ee0b929ddc486092db958f8b44
Aaron Rincon <rlaaron0305@gmail.com>
2022-10-10 12:25:13 -0500

    delete create webhooks in app.js

--- app.js
+++ app.js
@@ -1,20 +1,10 @@
 /* Importing the discord.js-selfbot-v13 and discord.js libraries. */
 const { Client } = require("discord.js-selfbot-v13")
-const { WebhookClient } = require("discord.js")
 require('dotenv').config();
+const {webhook1, webhook2, webhookErr, id1, id2} = require('./webhook.js');
 
 /* Getting the values from the .env file. */
 const usertoken = process.env.TOKEN;
-// add ids from the chats to listen to
-const id1 = process.env.ID1;
-const id2 = process.env.ID2;
-/* Creating a new webhook client for each webhook.
-    You can add more webhooks to listen the message in more chats.
-*/
-const webhook1 = new WebhookClient({ id: "1013201604326989964", token: "im2xMvDvflllcdmvcKNDjimwhExaQmJ7EkWmoi-dbiFM7fmjiIfcxJr9LsalCTglINVI" });
-const webhook2 = new WebhookClient({ id: "1013532925364535346", token: "AAEZFoOGSeTcJP8wi6Ijc_do-1HpkVKQs-Zph3zZAX-6kDZAxlhHCeOSkWN_i5GqiCan" });
-const webhookErr = new WebhookClient({ id: "1013967197590270022", token: "OlJFRgQ1ZVU8mXXjMV2nhbQzigbkXbg_4FnlfDqkwV5lrx673ZbACVLxobkIzvHApJro" });
-
 // add ids to the array
 const generalID = [id1, id2]
 // add webhooks to the array
@@ -27,7 +17,11 @@ client.login(usertoken);
 
 // When the client is ready, it will log "Ready!" in the console.
 client.on("ready", ready => {
-    console.log('Crypto Mirror ready')
+    console.log(`Crypto Mirror ${ready.presence.status}`);
+    if(ready.presence.status !== "online"){
+        client.destroy();
+        client.login(usertoken);
+    }
 });
 
 
@@ -44,15 +38,15 @@ client.on("messageCreate", messageCreate => {
             let username = messageCreate.author.username ? messageCreate.author.username : { embeds: [messageCreate.embeds[0]] };
             let avatarURL = messageCreate.author.avatarURL() ? messageCreate.author.avatarURL() : { embeds: [messageCreate.embeds[0]] };
             let contImg = messageCreate.attachments.size > 0 ? messageCreate.attachments.first().url : null;
-
             // check if the message has a txt
             contImg ? messageCreate.content? content += `\n${contImg}` : content = contImg: null; 
             // check if is a pinned message
             messageCreate.type === 'CHANNEL_PINNED_MESSAGE' ? content = `:pushpin: PINNED_MESSAGE` : null;
-            
+            content.length > 2000 ? content = content.slice(0, 2000) : null;
             // send the message to the webhook
             try{
-                webhooks[i].send({content, username, avatarURL });
+                if (typeof content !== 'string') throw new error(errorMessage);
+                    webhooks[i].send({content, username, avatarURL });
             }catch(err){
                 let errString = JSON.stringify(content);
                 errString = `Error: ${err}\n Objeto: \n${errString}`;
