975a84bb71e9fbd2c54b8c37a91da65a7c5f5fc7
Aaron Rincon <rlaaron0305@gmail.com>
2022-10-30 15:49:21 -0600

    create and export array

--- webhook.js
+++ webhook.js
@@ -11,6 +11,9 @@ const IDbot = process.env.IDBOT;
 const webhook1 = new WebhookClient({ id: "1013201604326989964", token: "im2xMvDvflllcdmvcKNDjimwhExaQmJ7EkWmoi-dbiFM7fmjiIfcxJr9LsalCTglINVI" });
 const webhook2 = new WebhookClient({ id: "1013532925364535346", token: "AAEZFoOGSeTcJP8wi6Ijc_do-1HpkVKQs-Zph3zZAX-6kDZAxlhHCeOSkWN_i5GqiCan" });
 const webhookErr = new WebhookClient({ id: "1013967197590270022", token: "OlJFRgQ1ZVU8mXXjMV2nhbQzigbkXbg_4FnlfDqkwV5lrx673ZbACVLxobkIzvHApJro" });
+
+const webhooks = [webhook1, webhook2, webhook1];
+const generalID = [id1, id2, IDgeneral];
 module.exports = {
     webhook1,
     webhook2,
@@ -18,5 +21,8 @@ module.exports = {
     id1,
     id2,
     IDgeneral,
-    IDbot
+    IDbot,
+    webhooks,
+    generalID
+
 }
